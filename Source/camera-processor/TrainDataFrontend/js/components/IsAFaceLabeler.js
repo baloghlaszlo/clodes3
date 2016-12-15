97f7ad949612312fed2a3525184e0bde507eca7c
Hegyi Bálint <baprof.hegyi@gmail.com>
2016-12-15 16:34:52 +0100

    Added left-right button handled

--- Source/camera-processor/TrainDataFrontend/js/components/IsAFaceLabeler.js
+++ Source/camera-processor/TrainDataFrontend/js/components/IsAFaceLabeler.js
@@ -17,32 +17,63 @@ export default class IsAFaceLabeler extends React.Component {
         }
     }
 
+    _handle_key(e) {
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            this._label(1)
+        } else if (e.keyCode == '39') {
+            this._label(0)
+        }
+    }
+
     render() {
-        return (
-            <div style={this.props.style}>
-                <div style={{
+        return ( <
+            div style = {
+                this.props.style
+            } >
+            <
+            div style = {
+                {
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'flex-start',
                     alignItems: 'center'
-                }}>
-                    <p>Is this a face?</p>
-                    <div>
-                        <RaisedButton
-                            label='Yes'
-                            primary={true}
-                            style={_buttonStyle}
-                            onTouchTap={() => this._label(1)}
-                        />
-                        <RaisedButton
-                            label='No'
-                            secondary={true}
-                            style={_buttonStyle}
-                            onTouchTap={() => this._label(0)}
-                        />
-                    </div>
-                </div>
-            </div>
+
+                }
+            }
+            onkeydown = {
+                (e) => this._handle_key(e)
+            } >
+            <
+            p > Is this a face ? < /p> <
+            div >
+            <
+            RaisedButton label = 'Yes'
+            primary = {
+                true
+            }
+            style = {
+                _buttonStyle
+            }
+            onTouchTap = {
+                () => this._label(1)
+            }
+            /> <
+            RaisedButton label = 'No'
+            secondary = {
+                true
+            }
+            style = {
+                _buttonStyle
+            }
+            onTouchTap = {
+                () => this._label(0)
+            }
+            /> <
+            /div> <
+            /div> <
+            /div>
         );
     }
 }
